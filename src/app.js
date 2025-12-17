f699fe4bb00982fcf4730b66b52fc70512be7ef2
Bohdan <dubchakbohdan63@gmail.com>
2025-12-18 00:52:05 +0100

    Add pub-sub pattern for automatic UI re-rendering

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
+import TaskStore from './state/taskStore';
 import { initTaskFrom } from './ui/taskForm';
 import { renderTasks } from './ui/renderTasks';
 
+const store = new TaskStore();
+
 function initApp() {
-    initTaskFrom();
-    renderTasks();
+    initTaskFrom(store);
+    renderTasks(store.addTask());
+
+    store.addEventListener('change', () => {
+        renderTasks(store.getTasks());
+    })
+
+    renderTasks(store.getTasks());
 }
 
 initApp();
