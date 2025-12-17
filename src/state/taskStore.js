a2e2e4287c06ccb15522071992ee5a434f5cf3b7
Bohdan <dubchakbohdan63@gmail.com>
2025-12-18 00:14:12 +0100

    Add StorageService and integrate persistent storage in TaskStore

--- src/state/taskStore.js
+++ src/state/taskStore.js
@@ -1,23 +1,36 @@
 import Task from './models/Task.js';
+import StorageService from '../services/storageService.js';
 
 export default class TaskStore {
     constructor() {
         this.tasks = [];
+        this.storage = new StorageService();
+        this.tasks = this.storage.load().map(taskData => {
+            const task = new Task(taskData.title);
+            task.status = taskData.status;
+            task.createAt = taskData.createAt;
+            return task;
+        });
     }
 
     addTask(title) {
         const task = new Task(title);
         this.tasks.push(task);
+        this.storage.save(this.tasks);
         return task;
     }
 
     removeTask(id) {
         this.tasks = this.tasks.find(task => task.id !== id);
+        this.storage.save(this.tasks);
     }
 
     updateTaskStatus(id, status) {
         const task = this.tasks.filter(task => task.id === id);
-        if (task) task.setStatus(status);
+        if (task) {
+            task.setStatus(status)
+            this.storage.save(this.tasks);
+        }
     }
 
     getTasks(filter = 'all') {
